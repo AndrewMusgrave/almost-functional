e0aa49d2374f9aad2fbd458f03e7b4ce2b73bef5
Andrew Musgrave <andrewdmusgrave@gmail.com>
2018-08-08 00:46:35 -0400

    Added rollup

---
+++ rollup.config.js
@@ -0,0 +1,27 @@
+import typescript from 'rollup-plugin-typescript2';
+import pkg from './package.json';
+
+const config = {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+    },
+    {
+      file: pkg.module,
+      format: 'es',
+    },
+  ],
+  external: [
+    ...Object.keys(pkg.dependencies || {}),
+    ...Object.keys(pkg.peerDependencies || {}),
+  ],
+  plugins: [
+    typescript({
+      typescript: require('typescript'),
+    }),
+  ],
+};
+
+export default config;
